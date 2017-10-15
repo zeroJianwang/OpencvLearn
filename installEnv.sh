3c889d9b18992478c2bc49cdf195cc3d3824278b
jianwang_zero <jianwang_zero@163.com>
2017-10-15 09:49:49 +0800

    fix mode

--- installEnv.sh
+++ installEnv.sh
@@ -2,15 +2,20 @@
 echo "***********************************************************"
 echo "*                                                        	*"
 echo "*								*"
-echo "*		Start to install some package for devolop	*"
+echo "*		Start to install some package for developer	*"
 echo "*								*"
 echo "*								*"
 echo "***********************************************************"
+apt-get update
 apt-get install vim
 apt-get install build-essetial
 apt-get install cmake
 apt-get install git
 apt-get insatll libgtk2.0-dev 
 apt-get install pkg-config
-apt-get insatll libavcodec-dev libavformat-dev libtiff.devlibswscale-dev libjasper-dev
+apt-get insatll libavcodec-dev
+apt-get install libavformat-dev 
+apt-get install libtiff4-dev
+apt-get install libswscale-dev 
+apt-get install libjasper-dev
 
