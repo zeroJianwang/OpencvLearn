15b11fab67eca9e85e7a0ec3d597560992298084
Zero <jianwang_zero@163.com>
2020-06-29 07:16:59 +0800

    add file

---
+++ train01.sh
@@ -0,0 +1,15 @@
+#!/user/bin/bash
+/*this is a */
+read -p "Please input a number:" number
+if[ "$number" = ^[0-9]+$ ];then
+        echo "what you input is not a number"
+        exit
+fi
+
+read -p "Please inut prefix:" preix
+if[];then
+fi
+
+if[] ; then
+
+fi
